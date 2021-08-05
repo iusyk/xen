75cda923a10b2f9c95a768cf8c9050132d71eb65
Valerii Chubar <valerii_chubar@epam.com>
2021-08-06 02:08:16 +0300

    xsm: add Flask policy for enable_rproc
    Introduce the flask access rule XEN_DOMCTL_enable_rproc.
    Allow dom0_t making the hypercalls in domD_t and domU_t contexts.
    
    Signed-off-by: Valerii Chubar <valerii_chubar@epam.com>
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- xen/xsm/flask/hooks.c
+++ xen/xsm/flask/hooks.c
@@ -751,6 +751,9 @@ static int flask_domctl(struct domain *d, int cmd)
     case XEN_DOMCTL_get_cpu_policy:
         return current_has_perm(d, SECCLASS_DOMAIN2, DOMAIN2__GET_CPU_POLICY);
 
+    case XEN_DOMCTL_enable_rproc:
+        return current_has_perm(d, SECCLASS_DOMAIN2, DOMAIN2__ENABLE_RPROC);
+
     default:
         return avc_unknown_permission("domctl", cmd);
     }
