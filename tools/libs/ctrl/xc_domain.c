cd563f253edec98901dd1aea94af7210570f71e4
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2021-12-08 15:35:24 +0200

    xen/arm: add SCI mediator support for DomUs
    Integration of the SCMI mediator with xen libs:
    - add hypercalls to aquire SCI channel and set device permissions
    for DomUs;
    - add SCMI_SMC nodes to DomUs device-tree based on partial device-tree;
    - SCI requests redirection from DomUs to Firmware.
    
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- tools/libs/ctrl/xc_domain.c
+++ tools/libs/ctrl/xc_domain.c
@@ -2188,6 +2188,24 @@ int xc_domain_getvnuma(xc_interface *xch,
     return rc;
 }
 
+
+int xc_domain_get_sci_info(xc_interface *xch, uint32_t domid,
+                            uint64_t *paddr, uint32_t *func_id)
+{
+    struct xen_domctl domctl = {};
+
+    memset(&domctl, 0, sizeof(domctl));
+    domctl.cmd = XEN_DOMCTL_get_sci_info;
+    domctl.domain = domid;
+
+    if ( do_domctl(xch, &domctl) != 0 )
+        return 1;
+
+    *paddr = domctl.u.sci_info.paddr;
+    *func_id = domctl.u.sci_info.func_id;
+    return 0;
+}
+
 int xc_domain_soft_reset(xc_interface *xch,
                          uint32_t domid)
 {
