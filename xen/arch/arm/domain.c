86b5ff0511bf27b6fcd495e69401c6fdac0efd8f
Ihor Usyk <ihor_usyk@epam.com>
2024-11-25 12:36:43 +0000

    domain.c: Remove the initialization of sci for  idle_domain.c
    Signed-off-by: Ihor Usyk <ihor_usyk@epam.com>

--- xen/arch/arm/domain.c
+++ xen/arch/arm/domain.c
@@ -776,7 +776,8 @@ int arch_domain_create(struct domain *d,
         if ( !vgic_reserve_virq(d, GUEST_EVTCHN_PPI) )
             BUG();
 #ifdef CONFIG_ARM_SCI
-        if ( config->arch.arm_sci_type != XEN_DOMCTL_CONFIG_ARM_SCI_NONE )
+        if ( !is_idle_domain(d) && 
+            config->arch.arm_sci_type != XEN_DOMCTL_CONFIG_ARM_SCI_NONE )
         {
             if ( (rc = sci_domain_init(d, config->arch.arm_sci_type,
                     &config->arch)) != 0)
