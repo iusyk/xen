a0282f6064211d01d893a0a500c5412f4393d627
Ihor Usyk <ihor_usyk@epam.com>
2024-11-26 15:34:13 +0000

    xen/arm: Export host device-tree to hypfs
    If enabled, host device-tree will be exported to hypfs and can be
    accessed through /devicetree path.
    Exported device-tree has the same format, as the device-tree
    exported to the sysfs by the Linux kernel.
    This is useful when XEN toolstack needs an access to the host device-tree.
    
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Signed-off-by: Ihor Usyk <ihor.usyk@gmail.com>

--- xen/common/hypfs.c
+++ xen/common/hypfs.c
@@ -376,13 +376,7 @@ int hypfs_read_dyndir_entry(const struct hypfs_entry *template,
     struct xen_hypfs_dirlistentry direntry;
     unsigned int e_len;
 
-    e_namelen = snprintf(name, sizeof(name), template->e.name, id);
-    if ( e_namelen >= sizeof(name) )
-    {
-        ASSERT_UNREACHABLE();
-        return -ENOBUFS;
-    }
-    e_len = DIRENTRY_SIZE(e_namelen);
+    e_len = DIRENTRY_SIZE(namelen);
     direntry.e.pad = 0;
     direntry.e.type = template->type;
     direntry.e.encoding = template->encoding;
