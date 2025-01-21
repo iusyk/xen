a4be0cbfc674d9ec377a5026e0ba351bbcd53235
Ihor Usyk <ihor_usyk@epam.com>
2025-01-21 14:50:01 +0000

    domctl.c

--- xen/common/domctl.c
+++ xen/common/domctl.c
@@ -284,13 +284,24 @@ long do_domctl(XEN_GUEST_HANDLE_PARAM(xen_domctl_t) u_domctl)
     bool copyback = false;
     struct xen_domctl curop, *op = &curop;
     struct domain *d;
-
+    printk(XENLOG_INFO"IHOR do_domctl\n");
     if ( copy_from_guest(op, u_domctl, 1) )
         return -EFAULT;
+    if(op->cmd == XEN_DOMCTL_get_sci_info)
+    {
+        printk(XENLOG_INFO"IHOR do_domctl starting get info\n");
+	printk(XENLOG_INFO"IHOR do_domctl interface %d, expected %d\n",  op->interface_version, XEN_DOMCTL_INTERFACE_VERSION);
+    }
 
     if ( op->interface_version != XEN_DOMCTL_INTERFACE_VERSION )
+    {
+	printk(XENLOG_ERR"IHOR do_domctl EACCESS !!!!!!!!!!!!!!!!\n");
         return -EACCES;
-
+    }
+    if(op->cmd == XEN_DOMCTL_get_sci_info)
+    {
+        printk(XENLOG_INFO"IHOR do_domctl starting get info , version ok\n");
+    }
     switch ( op->cmd )
     {
     case XEN_DOMCTL_assign_device:
@@ -859,6 +870,7 @@ long do_domctl(XEN_GUEST_HANDLE_PARAM(xen_domctl_t) u_domctl)
         break;
 
     default:
+	printk(XENLOG_INFO"IHOR from do_domctl call arch_do_domctl\n");
         ret = arch_do_domctl(op, d, u_domctl);
         break;
     }
